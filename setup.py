781c5d1d6356f9a7e9565d6a233a797d8b41bcbf
dipanjanb <dipanjanb@gmail.com>
2024-01-04 23:07:58 +0530

    added package versions

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     packages=find_packages(where='src'),
     install_requires=[
         "profiles-rudderstack==0.10.5",
-        "langchain",
-        "pandas",
-        "openai",
-        "langchain-google-genai",
-        "boto3",
-        "awscli",
-        "botocore"
+        "langchain>=0.0.352",
+        "pandas>=2.0.3",
+        "openai>=1.6.1",
+        "langchain-google-genai>=0.0.5",
+        "boto3>=1.34.12",
+        "awscli>=1.32.12",
+        "botocore>=1.34.12"
     ]
 )
