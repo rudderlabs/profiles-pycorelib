12418bb77e03ac476c077bab8212c7550bbe498f
yajuaryarudderstack <102648727+yajuaryarudderstack@users.noreply.github.com>
2024-02-29 17:38:04 +0530

    chore: main sync release feb2024v0.11.0 (#13)
    * chore: update core lib models
    
    * chore: update pkg version
    
    * fix: relax dep on profiles-rudderstack ver
    
    * Version 0.11.0rc1 for release-candidate.
    
    * chore: add make release and make install (#11)
    
    * chore: add make release and make install
    
    * chore: add comment
    
    * chore: update makefile add version
    
    * fix: small change
    
    * fix: make release tag missing v (#12)
    
    * Version 0.11.0rc4 for release-candidate.
    
    * v0.11.0rc4 release
    
    * Version 0.11.0rc5 for release-candidate.
    
    * v0.11.0rc5 release
    
    * Version 0.11.0rc6 for release-candidate.
    
    * v0.11.0rc6 release
    
    * Version 0.11.0rc7 for release-candidate.
    
    * v0.11.0rc7 release
    
    * Version 0.11.0rc8 for release-candidate.
    
    * v0.11.0rc8 release
    
    * Version 0.11.0 for release.
    
    * v0.11.0 release
    
    ---------
    
    Co-authored-by: Shubham Mehra <54092258+shubhammehra4@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 from setuptools import setup, find_packages
+from version import version
+
+
+def limit_major_minor_version(version: str) -> str:
+    """
+    Returns a modified version string that limits the major and minor version numbers.
+
+    Args:
+        version (str): The original version string.
+
+    Returns:
+        str: The modified version string with only the major and minor version numbers.
+
+    Example:
+        >>> limit_major_minor_version("1.2.3")
+        '1.2.*'
+    """
+    return ".".join(version.split(".")[:2]) + ".*"
+
 
 setup(
     name='profiles-pycorelib',
-    version='0.2.2',
+    version=version,
     author='Shubham Mehra',
     author_email='shubhammehra@ruddersatck.com',
     description='A Python Native package that registers the core python models',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=[
-        "profiles-rudderstack>=0.10.5, <=0.10.7, !=0.10.6",
+        f"profiles-rudderstack=={limit_major_minor_version(version)}",
     ]
 )
