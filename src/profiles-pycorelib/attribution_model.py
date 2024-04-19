7c532c92ba6bf6c1bc8f99673a416e69932538f8
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2024-04-19 17:46:17 +0530

    parsing comma separated touchpoints string

--- src/profiles-pycorelib/attribution_model.py
+++ src/profiles-pycorelib/attribution_model.py
@@ -256,8 +256,8 @@ class AttributionModelRecipe(PyNativeRecipe):
         filtered_df.columns = [x.lower() for x in input_df.columns]
         def _convert_str_to_list(x):
             try:
-                return ast.literal_eval(x)
-            except ValueError:
+                return x.split(",")
+            except:
                 return []
         filtered_df[touch_point_var] = filtered_df[touch_point_var].apply(_convert_str_to_list)
 
