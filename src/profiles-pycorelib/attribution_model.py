b2c69be11935d5ed93e84c3c8c76669593d90c84
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2024-06-25 11:48:47 +0530

    reverting converge() from torch to scipy

--- src/profiles-pycorelib/attribution_model.py
+++ src/profiles-pycorelib/attribution_model.py
@@ -6,7 +6,7 @@ from profiles_rudderstack.material import WhtMaterial
 from profiles_rudderstack.logger import Logger
 import seaborn as sns
 import matplotlib
-import torch
+import scipy.linalg as linalg
 import plotly.graph_objects as go
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
@@ -134,24 +134,25 @@ class MultiTouchModels:
         return transition_probabilities, labels
 
     def _converge(self, transition_matrix, max_iters=40, verbose=False):
-        transition_matrix = torch.tensor(transition_matrix, dtype=torch.float32)
-        T_upd = transition_matrix
-        prev_T = transition_matrix
-
+        transition_matrix = np.array(transition_matrix, dtype=np.float32)
+        T_upd = transition_matrix.copy()
+        prev_T = transition_matrix.copy()
         for i in range(max_iters):
-            T_upd = torch.matmul(transition_matrix, prev_T)
-            if torch.abs(T_upd - prev_T).max() < 1e-3:
+            T_upd = linalg.blas.dgemm(1.0, transition_matrix, prev_T)
+            if np.abs(T_upd - prev_T).max() < 1e-3:
                 if verbose:
                     self.logger.info(f"{i} iters taken for convergence")
-                return T_upd.numpy()
+                return T_upd
             prev_T = T_upd
         if verbose:
             self.logger.info(f"Max iters of {max_iters} reached before convergence. Exiting")
-        return T_upd.numpy() 
+        return T_upd
     
     def _get_removal_affects(self, transition_probs, labels, ignore_labels=["Start", "Dropoff","Converted"], default_conversion=1.):
         removal_affect = {}
         for n, label in enumerate(labels):
+            if n % (len(labels)//10) == 0:
+                self.logger.info(f"Computing removal affects: {n/len(labels)*100:.2f}%")
             if label in ignore_labels:
                 continue
             else:
